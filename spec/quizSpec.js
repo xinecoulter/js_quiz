210986e31f94a14a92c0ac30463ec6f633ec380d
Christine Coulter <christinetcoulter@gmail.com>
2013-08-22 10:01:14 -0400

    initial commit

--- spec/quizSpec.js
+++ spec/quizSpec.js
@@ -25,3 +25,58 @@ describe('question1', function() {
 
 // Write the next tests yourself!
 // See quiz.js for more details
+describe('question2', function() {
+  it('returns true if the number is between 90 and 99', function() {
+    expect(quiz.nearHundred(93)).toBe(true);
+  });
+
+  it('returns false if the number is 89 or below', function() {
+    expect(quiz.nearHundred(52)).toBe(false);
+  });
+
+  it('throws an error if the argument passed is not a number', function() {
+    expect(quiz.nearHundred('two')).toThrow(new Error("Please enter a number!"));
+  });
+});
+
+describe('question3', function() {
+  it('removes the character that corresponds to the index from the string', function() {
+    expect(quiz.missingChar("kittie", 1)).toEqual("kttie");
+  });
+
+  it('throws an error if a string is not entered', function() {
+    expect(quiz.missingChar(347, 1)).toThrow(new Error("Please enter a string!"));
+  });
+
+});
+
+describe('question4', function() {
+
+  it("removes 'del' from a string if 'del' is present", function() {
+    expect(quiz.delDel("abdelcd")).toEqual("abcd");
+  });
+
+  it("does not affect the string if 'del' is not present", function() {
+    expect(quiz.delDel("xyz")).toEqual("xyz");
+  });
+
+});
+
+describe('question5', function() {
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/quiz.js
+++ src/quiz.js
@@ -11,8 +11,14 @@ var quiz = {};
 // You can sleep in if it is not a weekday or if you are on vacation.
 // quiz.sleepIn() => false
 // quiz.sleepIn({vacation: true}) => true
+var current_day = "Thursday";
 quiz.sleepIn = function(options) {
-}
+  if ((options.vacation === true) || (current_day === "Saturday") || (current_day === "Sunday")){
+    return true;
+  } else {
+    return false;
+  }
+};
 
 // Question 2: function called nearHundred
 //////////////
@@ -21,13 +27,32 @@ quiz.sleepIn = function(options) {
 // quiz.nearHundred(52) => false
 // quiz.nearHundred(93) => false
 // quiz.nearHundred('two') => Error: Please enter a number!
-
+quiz.nearHundred = function(number) {
+  if (typeof(number) === "number") {
+    if ((number >= 90) && (number <= 99)) {
+      return true;
+    } else if (number < 90) {
+      return false;
+    }
+  } else {
+    throw "Error: Please enter a number!";
+  }
+};
 // Question 3: a function called missingChar
 //////////////
 // Remove the character that corresponds to the index from the string.
 // If you don't enter a string
 // quiz.missingChar("kittie", 1) => "kttie"
 // quiz.missingChar(347, 1) => Error: Please enter a string!
+quiz.missingChar = function(string, index) {
+  if (typeof(string) === "string") {
+    function replaceAt(string, index) {
+      return string.substring(0, index) + string.substring(index + 1);
+    };
+  } else {
+    throw "Error: Please enter a string!";
+  }
+};
 
 // Question 4: a function called delDel
 //////////////
