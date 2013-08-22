d56d4761825d6be047a27c9a1c517dc90abba2dd
Phillip Lamplugh <phillip.lamplugh@gmail.com>
2013-08-21 22:24:26 -0400

    first commit

---
+++ spec/quizSpec.js
@@ -0,0 +1,24 @@
+//      ___       _______      ___       __  .__   __.  ______    __
+//     /   \     /  _____|    /   \     |  | |  \ |  | |      \  |  |
+//    /  ^  \   |  |  __     /  ^  \    |  | |   \|  | `----)  | |  |
+//   /  /_\  \  |  | |_ |   /  /_\  \   |  | |  . `  |     /  /  |  |
+//  /  _____  \ |  |__| |  /  _____  \  |  | |  |\   |    |__|   |__|
+// /__/     \__\ \______| /__/     \__\ |__| |__| \__|     __    (__)
+//                                                        (__)
+// http://pivotal.github.io/jasmine/
+
+// The first question's tests are written.
+// See quiz.js for more details
+
+describe('question1', function() {
+
+  it('returns true if today is saturday or sunday', function() {
+    // Assuming you're taking this on a weekday!
+    expect(quiz.sleepIn()).toBe(false);
+  });
+
+  it('returns true if an options hash is passed with vacation set to true', function() {
+    expect(quiz.sleepIn({vacation: true})).toBe(true);
+  });
+
+});
---
+++ src/quiz.js
@@ -0,0 +1,42 @@
+//  .___________. __    __   __       _______.    __       _______.        ___           ______      __    __   __   ________
+//  |           ||  |  |  | |  |     /       |   |  |     /       |       /   \         /  __  \    |  |  |  | |  | |       /
+//  `---|  |----`|  |__|  | |  |    |   (----`   |  |    |   (----`      /  ^  \       |  |  |  |   |  |  |  | |  | `---/  /
+//      |  |     |   __   | |  |     \   \       |  |     \   \         /  /_\  \      |  |  |  |   |  |  |  | |  |    /  /
+//      |  |     |  |  |  | |  | .----)   |      |  | .----)   |       /  _____  \     |  `--'  '--.|  `--'  | |  |   /  /----.__
+//      |__|     |__|  |__| |__| |_______/       |__| |_______/       /__/     \__\     \_____\_____\\______/  |__|  /________(__)
+var quiz = {};
+
+// Question 1: a function called sleepIn
+//////////////
+// You can sleep in if it is not a weekday or if you are on vacation.
+// quiz.sleepIn() => false
+// quiz.sleepIn({vacation: true}) => true
+quiz.sleepIn = function(options) {
+}
+
+// Question 2: function called nearHundred
+//////////////
+// If the number is between 90 and 99, the result is true;
+// If it is 89 or below, it is false.
+// quiz.nearHundred(52) => false
+// quiz.nearHundred(93) => false
+// quiz.nearHundred('two') => Error: Please enter a number!
+
+// Question 3: a function called missingChar
+//////////////
+// Remove the character that corresponds to the index from the string.
+// If you don't enter a string
+// quiz.missingChar("kittie", 1) => "kttie"
+// quiz.missingChar(347, 1) => Error: Please enter a string!
+
+// Question 4: a function called delDel
+//////////////
+// Remove "del" from a string.
+// quiz.delDel("abdelcd") => "abcd"
+// quiz.delDel("xyz") => "xyz"
+
+// Question 5: a method called backAround
+//////////////
+// Given a string, move the last character to the beginning.
+// "cat".backAround() => "tca"
+// "hello".backAround() => "ohell"
